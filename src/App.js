cb6beeae14f46405479d5731a7dd91c2040034bb
Denise Tran <tdenise@users.noreply.github.com>
2020-11-16 22:52:09 -0800

    Base layout of website, no css applied yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+import { NavBar } from './components/NavBar';
+import { InfoCard } from './components/Card';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Container class="Layout-container">
+        <Row>
+          <Col>      
+            <InfoCard />
+          </Col>
+          <Col>...</Col> 
+        </Row>
+      </Container>
     </div>
   );
 }
