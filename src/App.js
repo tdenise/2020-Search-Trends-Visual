f415772e86ae5cef34e42f6f39baa4c7241ec60d
tdenise <t.denise44@yahoo.com>
2020-11-21 15:18:25 -0800

    create data viz and sections for each month

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import { NavBar } from './components/NavBar';
 // import { InfoCard } from './components/Card';
 // import { Container, Row, Col } from 'react-bootstrap';
 import { TestGraph } from './components/TestGraph';
+import { JanGraph } from './components/JanGraph';
+import { FebGraph } from './components/FebGraph';
+import { MarGraph } from './components/MarGraph';
+import { AprGraph } from './components/AprGraph';
+import { MayGraph } from './components/MayGraph';
+import { JunGraph } from './components/JunGraph';
+import { JulGraph } from './components/JulGraph';
+import { AugGraph } from './components/AugGraph';
+import { SepGraph } from './components/SepGraph';
+import { OctGraph } from './components/OctGraph';
+import { NovGraph } from './components/NovGraph';
 import { JumbotronSect } from './components/Jumbotron';
 
 function App() {
@@ -15,6 +26,17 @@ function App() {
       <NavBar />
       <JumbotronSect />
       <TestGraph />
+      <JanGraph />
+      <FebGraph />
+      <MarGraph />
+      <AprGraph />
+      <MayGraph />
+      <JunGraph />
+      <JulGraph />
+      <AugGraph />
+      <SepGraph />
+      <OctGraph />
+      <NovGraph />
       
     </div>
   );
